3c42f60dca1fab3f15a8869c68680b746e6862fc
Calvin <calvinsoares17@gmail.com>
2024-04-02 11:49:38 -0300

    feat: server express created

---
+++ src/server.js
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import express from 'express';
+import cors from 'cors';
+import router from './routes/routes.js';
+
+const app = express();
+const PORT = 3000;
+
+dotenv.config();
+
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@dashboard.ta1i6cz.mongodb.net/?retryWrites=true&w=majority&appName=dashboard`)
+    .then(() => {
+        app.listen(PORT)
+        console.log('Conectou ao Banco')
+    })
+    .catch((err) => console.log(err))
+
+app.use(express.json())
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}))
+
+app.use('/', router)
